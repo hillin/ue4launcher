5093710328f498659cadaee3da58c44ebea2a566
hillin <hillinsilence@gmail.com>
2016-11-29 15:31:46 +0800

    add CPU and memory usage display for processes

--- ProjectLauncher/Processes/ProcessViewModel.cs
+++ ProjectLauncher/Processes/ProcessViewModel.cs
@@ -1,9 +1,11 @@
-﻿using System.Diagnostics;
+﻿using System;
+using System.Diagnostics;
+using System.Windows.Threading;
 using UE4Launcher.Debugging;
 
 namespace UE4Launcher.Processes
 {
-    class ProcessViewModel
+    class ProcessViewModel : NotificationObject
     {
         private readonly Process _process;
 
@@ -13,9 +15,66 @@ namespace UE4Launcher.Processes
         public string Name => _process.ProcessName;
         public string StartTime => _process.StartTime.ToLongTimeString();
 
+        private TimeSpan _lastTotalProcessorTime;
+        private DateTime _lastProfilingTime;
+
+        private string _cpuUsageDisplay;
+
+        public string CPUUsageDisplay
+        {
+            get { return _cpuUsageDisplay; }
+            private set
+            {
+                _cpuUsageDisplay = value;
+                this.RaisePropertyChanged(nameof(this.CPUUsageDisplay));
+            }
+        }
+
+
+        private string _memoryDisplay;
+        public string MemoryDisplay
+        {
+            get { return _memoryDisplay; }
+            private set
+            {
+                _memoryDisplay = value;
+                this.RaisePropertyChanged(nameof(this.MemoryDisplay));
+            }
+        }
+
         public ProcessViewModel(Process process)
         {
             _process = process;
+            var timer = new DispatcherTimer(DispatcherPriority.DataBind)
+            {
+                Interval = TimeSpan.FromSeconds(1)
+            };
+
+            timer.Tick += this.ProfilingUpdateTimer_Tick;
+            timer.Start();
+            this.UpdateProfiling();
+        }
+
+        private void ProfilingUpdateTimer_Tick(object sender, EventArgs e)
+        {
+            this.UpdateProfiling();
+        }
+
+        private void UpdateProfiling()
+        {
+            var now = DateTime.UtcNow;
+            var totalProcessorTime = _process.TotalProcessorTime;
+            var cpuUsage = (totalProcessorTime.TotalMilliseconds - _lastTotalProcessorTime.TotalMilliseconds)
+                           / now.Subtract(_lastProfilingTime).TotalMilliseconds
+                           / Environment.ProcessorCount;
+
+            this.CPUUsageDisplay = cpuUsage.ToString("P1");
+
+            var memory = (double)_process.WorkingSet64 / 1024 / 1024;
+            this.MemoryDisplay = memory > 1024 ? $"{memory / 1024:F1} GiB" : $"{memory:F0} MiB";
+
+            _lastProfilingTime = now;
+            _lastTotalProcessorTime = totalProcessorTime;
         }
 
         public void Kill()
